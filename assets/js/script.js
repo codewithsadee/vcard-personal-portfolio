9821be94bb7239f1dbfa495c5132b50fd554b7e9
Alex <alexrumer@gmail.com>
2024-01-12 13:03:53 -0800

    Description and portfilo
    Added portfolio and updated description.

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,38 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+// Add event listener to project container
+const projectListEl = document.querySelector(".project-list");
+const openProjectImgs = projectListEl.querySelectorAll(".project-img");
+const modalDialogEl = document.querySelector("dialog");
+const closeModalDialogEl = document.querySelector("#close");
+const modalImgEl = document.querySelector(".modal-body > img");
+
+// adding click event on every project
+for (let i = 0; i < openProjectImgs.length; i++) {
+  const openProjectImgEl = openProjectImgs[i];
+  openProjectImgEl
+    .querySelector(".project-item-icon-box")
+    .addEventListener("click", () => {
+      modalImgEl.src = openProjectImgEl.querySelector("img").src;
+      modalDialogEl.showModal();
+    });
+}
+
+// close btn handler
+closeModalDialogEl.addEventListener("click", (event) =>
+  modalCloseHandler(event, ["ION-ICON"])
+);
+
+// backdrop close handler
+modalDialogEl.addEventListener("click", (event) =>
+  modalCloseHandler(event, ["DIALOG"])
+);
+
+function modalCloseHandler(event, nodeName) {
+  if (nodeName.includes(event.target.nodeName)) {
+    modalImgEl.src = "";
+    modalDialogEl.close();
+  }
+}
